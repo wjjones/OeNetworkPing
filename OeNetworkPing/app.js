b21b34b789d1e163b46cf6b611ea4c17d055ea04
Jeff Jones <wj_jones@msn.com>
2018-08-15 14:48:17 -0400

    Start of Program

--- OeNetworkPing/app.js
+++ OeNetworkPing/app.js
@@ -1,3 +1,82 @@
 'use strict';
+const fs = require('fs');
+const ini = require('ini');
+const netping = require('net-ping');
 
-console.log('Hello world');
+// Read in the ini
+const setup = ini.parse(fs.readFileSync('./setup.ini', 'utf-8'));
+// Assign the vars to the Keys from the ini file
+// for net-ping
+const ininetworkProtocol = Object.keys(setup.networkProtocol);
+const inipacketSize = Object.keys(setup.packetSize);
+const iniretries = Object.keys(setup.retries);
+const inisessionid = Object.keys(setup.sessionid);
+const initimeout = Object.keys(setup.timeout);
+const inittl = Object.keys(setup.ttl);
+// for program
+const inipingfreq = Object.keys(setup.pingfreq);
+const inioeserver = Object.keys(setup.oeserver);
+const inioedatabasename = Object.keys(setup.oedatabasename);
+const inidevices = Object.keys(setup.devices);
+const inicompleteoeservername = inioeserver + ":" + inioedatabasename;
+//const ini = Object.keys(setup.);
+
+console.log("ininetworkProtocol : " + ininetworkProtocol);
+console.log("inipacketSize : " + inipacketSize);
+console.log("iniretries : " + iniretries);
+console.log("inisessionid : " + inisessionid);
+console.log("initimeout : " +initimeout);
+console.log("inittl : " + inittl);
+console.log("inipingfreq : " + inipingfreq);
+console.log("inioeserver : " + inioeserver);
+console.log("inioedatabasename : " + inioedatabasename);
+console.log("inidevices : " + inidevices);
+console.log("inicompleteoeservername : " + inicompleteoeservername);
+
+console.log("Net-Ping will check: " + inidevices + '\n' +
+    " using: " + ininetworkProtocol + '\n' +
+    " With a packet size of: " + inipacketSize + '\n' +
+    " a retry of : " + iniretries + '\n' +
+    " a timeout of : " + initimeout + '\n' +
+    " a time to live of : " + inittl + '\n' +
+    "It will log the Data to : " + inicompleteoeservername );
+
+
+var options = {
+    retries: iniretries,
+    timeout: initimeout
+};
+
+var session = netping.createSession(options);
+
+session.on("error", function (error) {
+    console.trace(error.toString());
+});
+
+for (var i = 0; i < inidevices.length; i++) {
+
+    session.pingHost(inidevices[i], function (error, inidevices, sent, rcvd) {
+
+        var ms = rcvd - sent;
+
+        if (error)
+
+            if (error instanceof netping.RequestTimedOutError)
+
+                console.log(inidevices + ": Not alive (ms=" + ms + ")");
+
+            else
+
+                console.log(inidevices + ": " + error.toString() + " (ms="
+
+                    + ms + ")");
+
+        else
+
+            console.log(inidevices + ": Alive alive (ms=" + ms + ")");
+
+    });
+
+}
+
+
